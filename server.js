568f45ea38d7da60ec7226d42b2d22e2a2a3b83a
Miao Jiang <mijiang@microsoft.com>
2021-03-18 13:06:43 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const WebSocket = require('ws'); // new
+const app = express();
+// express code 
+const socketServer = new WebSocket.Server({port: 3030});
+socketServer.on('connection', (socketClient) => {
+  console.log('connected');
+  console.log('client Set length: ', socketServer.clients.size);
+
+  socketClient.on('close', (socketClient) => {
+    console.log('closed');
+    console.log('Number of clients: ', socketServer.clients.size);
+  });
+
+  socketClient.on('message', (message) => {
+    socketClient.send(message);
+    
+  });
+
+
+});
